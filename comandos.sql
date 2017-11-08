00e11c5931f76c91b849c6668a47444fd31267cd
Kamile Pimenta <kamile.pimenta@outlook.com>
2017-11-08 17:09:34 -0200

    Banco de Dados: Modelagem

--- comandos.sql
+++ comandos.sql
@@ -26,3 +26,29 @@ INSERT INTO nome_da_tabela
 VALUES
   ('valor', 'valor2', 'valor3'),
   ('valor', 'valor2', 'valor3');
+
+-- Atualizando Registros
+UPDATE nome_da_tabela
+SET nome_campo = 'valor', nome_campo2 = 'valor'
+WHERE nome_campo3 > 10;
+
+-- Deletando Registros
+DELETE FROM nome_da_tabela WHERE nome_campo3 IS NULL;
+
+-- Apagando e Inserindo Registros em um único comando
+REPLACE INTO nome_da_tabela
+  (nome_campo_chave_primaria, nome_campo2, nome_campo3)
+VALUES
+  ('valor', 'valor2', 'valor3'),
+  ('valor', 'valor2', 'valor3');
+-- Obs.: Para que o REPLACE funcione como o esperado, o campo de Chave Primária deve ser informado entre os campos
+
+-- Mostrando Registros
+SELECT nome_campo, nome_campo2, nome_campo3
+FROM nome_da_tabela
+WHERE condicional
+GROUP BY campos
+HAVING filtro
+ORDER BY campos
+LIMIT offset,qtde;
+
